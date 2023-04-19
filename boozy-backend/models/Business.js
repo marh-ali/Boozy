ff163445bcdabb7e62f575526e5836eec208945b
marh-ali <aam261@Georgetown.edu>
2023-04-18 22:44:40 -0400

    Merged two tables, they were redundant

--- boozy-backend/models/Business.js
+++ boozy-backend/models/Business.js
@@ -1,45 +1,48 @@
 const mongoose = require("mongoose");
 
-const BusinessSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  location: {
-    address: {
+const BusinessSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
       required: true,
     },
-    city: {
+    email: {
       type: String,
       required: true,
     },
-    state: {
+    password: {
       type: String,
       required: true,
     },
-    country: {
-      type: String,
-      required: true,
+    location: {
+      address: String,
+      city: String,
+      state: String,
+      country: String,
+      postalCode: String,
+      coordinates: {
+        type: [Number],
+        index: "2dsphere",
+      },
     },
-    postalCode: {
-      type: String,
-      required: true,
-    },
-    coordinates: {
-      type: [Number],
-      required: true,
-      index: "2dsphere",
+    happyHour: {
+      offersFood: Boolean,
+      offersDrinks: Boolean,
+      times: [
+        {
+          dayOfWeek: String,
+          start: String,
+          end: String,
+        },
+      ],
+      menu: String,
     },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("Business", BusinessSchema);
+const Business = mongoose.model("Business", BusinessSchema);
+
+module.exports = Business;
