024d202caf94c05d32c9e95362be60e169a08d55
marh-ali <aam261@Georgetown.edu>
2023-04-18 21:23:45 -0400

    Updated business schema

--- boozy-backend/models/Business.js
+++ boozy-backend/models/Business.js
@@ -13,6 +13,33 @@ const BusinessSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  location: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: "2dsphere",
+    },
+  },
 });
 
 module.exports = mongoose.model("Business", BusinessSchema);
